acb129046a99ef7100a6caf1b0fd9c7af7a725d2
tongnamuu <thban33@naver.com>
2020-03-01 21:29:34 +0900

    A Middleware Added

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ const app = express();
 const handleListening = () => {
   console.log("Listening on: http://localhost:4000");
 };
-app.listen(4000, handleListening);
 
 const handleHome = (req, res) => res.send("This is Home Page");
 
-app.get("/", handleHome);
+const handleProfile = (req, res) => res.send("This is Profile Page");
 
 // function handleProfile(req, res) {
 //   res.send("This is Profile Page");
 // }
-const handleProfile = (req, res) => res.send("This is Profile Page");
 
+const betweenHome = (req, res, next) => {
+  console.log("I'm between Something");
+  next();
+};
+
+app.use(betweenHome);
+app.get("/", handleHome);
 app.get("/profile", handleProfile);
+app.listen(4000, handleListening);
