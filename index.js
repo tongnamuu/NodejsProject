e2c3bd691153901afdbcad91a803cd92501fa576
tongnamuu <thban33@naver.com>
2020-03-01 21:41:16 +0900

    Middleware Setup Completed

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 import express from "express";
 //const express = require("express");
-const app = express();
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
+const app = express();
+const PORT = 4000;
 const handleListening = () => {
-  console.log("Listening on: http://localhost:4000");
+  console.log(`Listening on: http://localhost:${PORT}`);
 };
 
 const handleHome = (req, res) => res.send("This is Home Page");
 
 const handleProfile = (req, res) => res.send("This is Profile Page");
 
-// function handleProfile(req, res) {
-//   res.send("This is Profile Page");
-// }
+//Middleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev")); //logging
+app.use(helmet()); //security
 
-const betweenHome = (req, res, next) => {
-  console.log("I'm between Something");
-  next();
-};
-
-app.use(betweenHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
-app.listen(4000, handleListening);
+
+app.listen(PORT, handleListening);
