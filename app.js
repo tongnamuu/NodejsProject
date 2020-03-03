e5a793078b6ed522c8137785fd89f09c26d0ea4f
tongnamuu <thban33@naver.com>
2020-03-03 22:35:37 +0900

    Router Working on

--- app.js
+++ app.js
@@ -4,12 +4,8 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { userRouter } from "./router";
 const app = express();
-const PORT = 4000;
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-};
 
 const handleHome = (req, res) => res.send("This is Home Page");
 
@@ -25,4 +21,6 @@ app.use(helmet()); //security
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
