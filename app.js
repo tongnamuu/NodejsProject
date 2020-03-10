176e2215e2e454927bbfaea08a26ab01857c4c8d
tongnamuu <thban33@naver.com>
2020-03-10 12:17:47 +0900

    MVC pattern Working on

--- app.js
+++ app.js
@@ -4,23 +4,28 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
-const handleHome = (req, res) => res.send("This is Home Page");
+//const handleHome = (req, res) => res.send("This is Home Page");
 
-const handleProfile = (req, res) => res.send("This is Profile Page");
+//const handleProfile = (req, res) => res.send("This is Profile Page");
 
 //Middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev")); //logging
 app.use(helmet()); //security
+app.use(morgan("dev")); //logging
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+//app.get("/", handleHome);
+//app.get("/profile", handleProfile);
 
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
