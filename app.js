5a3069e0b9ea710cd9d7f81d27218883ac9b8b2d
tongnamuu <thban33@naver.com>
2020-03-14 13:13:30 +0900

    PUG SETTING COMPLETED

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 const app = express();
 
 //Middleware
+app.use(helmet()); //security
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); //security
 app.use(morgan("dev")); //logging
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
